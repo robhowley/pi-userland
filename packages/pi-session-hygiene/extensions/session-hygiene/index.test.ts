383a8df4c4af3536527687e2c347a69654abc867
Rob Howley <rob.howley@shopify.com>
2026-04-29 14:44:24 -0400

    fix(lint): add eslint-disable for ban-ts-comment in test file

--- packages/pi-session-hygiene/extensions/session-hygiene/index.test.ts
+++ packages/pi-session-hygiene/extensions/session-hygiene/index.test.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import sessionHygieneExtension from './index.js';
