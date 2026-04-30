f8847d7bfb3906104ba1f4c202a1f2a5496fb2c3
Rob Howley <rob.howley@shopify.com>
2026-04-30 16:43:17 -0400

    chore: clean up vendor nonsense

--- packages/pi-structured-return/benchmarks/test-runners/cargo/target/debug/deps/math-649e5818cfa2888e.d
+++
@@ -1,5 +0,0 @@
-/Users/roberthowley/src/github.com/pi-structured-return/benchmarks/test-runners/cargo/target/debug/deps/math-649e5818cfa2888e.d: src/lib.rs
-
-/Users/roberthowley/src/github.com/pi-structured-return/benchmarks/test-runners/cargo/target/debug/deps/math-649e5818cfa2888e: src/lib.rs
-
-src/lib.rs:
